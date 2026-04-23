787869e6ec9f433c6f95adb8f560ca4219bd110a
anshrajput277-prog <ansh.rajput277@gmail.com>
2026-04-23 11:08:14 +0530

    config: update API URL for production

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://lost-and-found-management-system-t0cs.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
